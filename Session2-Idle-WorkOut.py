632c9db6c3a3700047eb4e9e50a3fd160f56ffe3
Sri Sreedhar <srisreedhar@live.com>
2020-03-18 10:10:36 +0530

    added IDLE workout

---
+++ Session2-Idle-WorkOut.py
@@ -0,0 +1,95 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 4
+4
+>>> 1111
+1111
+>>> 89.567
+89.567
+>>> "text data"
+'text data'
+>>> in
+SyntaxError: invalid syntax
+>>> dir()
+['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__']
+>>> list()
+[]
+>>> for
+SyntaxError: invalid syntax
+>>> 2+2
+4
+>>> 4+"good morning"
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    4+"good morning"
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 
+>>> 
+>>> # Arithmetic Operators
+>>> # add - +
+>>> # subtraction -
+>>> # division - /
+>>> # multiply - *
+>>> # operators - more than 1 operand
+>>> 
+>>> # arithmetic operations -> applied on same input data/supported data
+>>> 
+>>> 2+345+2.5+100
+449.5
+>>> 
+>>> # " string/text "
+>>> 
+>>> 1111 + 9999
+11110
+>>> "1111" +"9999"
+'11119999'
+>>> 
+>>> 
+>>> "elon"+"musk"
+'elonmusk'
+>>> # concatenation
+>>> # "elon"+"musk"
+>>> 
+>>> "elon"+"  "+"musk"
+'elon  musk'
+>>> 
+>>> 'elon'+"999  111"+'musk'+"  "+" is "
+'elon999  111musk   is '
+>>> ''' 3 single quotes '''
+' 3 single quotes '
+>>> 
+>>> """ 3 double quotes """
+' 3 double quotes '
+>>> 
+>>> 
+>>> # Variables
+>>> 
+>>> # create memory / store values in the program session/space
+>>> 
+>>> # LHS = RHS
+>>> # LeftHand Side = RightHand Side
+>>> 
+>>> # VariableName = Value
+>>> 
+>>> firstname="sri"
+>>> lastname="sree"
+>>> age=10
+>>> email="srisree@"
+>>> age=12
+>>> 
+>>> 
+>>> age
+12
+>>> lastname
+'sree'
+>>> firstname + lastname
+'srisree'
+>>> # variables = aliases / label / identifier
+>>> 
+>>> # basic calculations
+>>> # multiplication
+>>> # "text" * 3
+>>> # " text" * float
+>>> # "textis" / 3
+>>> 
+>>> 
