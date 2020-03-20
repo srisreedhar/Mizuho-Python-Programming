a68b550694d3a5f620e8b82e2fbd3607e84d3e15
Sri Sreedhar <srisreedhar@live.com>
2020-03-20 07:51:15 +0530

    March-20

---
+++ Session3-Idle-WorkOut.py
@@ -0,0 +1,129 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # LHS = RHS
+>>> # VariableName = Values
+>>> # lowercase - everything else
+>>> # UPPERCASE - global declarations
+>>> 
+>>> # camelCase - functions
+>>> # UpperCamelCase - ClassNames
+>>> 
+>>> # details of a person
+>>> # name,age,edu,contact
+>>> "mark twain"
+'mark twain'
+>>> name="Nokola Tesla"
+>>> name
+'Nokola Tesla'
+>>> name="NokolaTesla"
+>>> name
+'NokolaTesla'
+>>> # over riding - assigning new values to an old variable
+>>> shareprice=3.45
+>>> shareprice=3.49
+>>> shareprice=6
+>>> 
+>>> age=10
+>>> edu="MBA"
+>>> contact="1234567"
+>>> 
+>>> 
+>>> name
+'NokolaTesla'
+>>> age
+10
+>>> edu
+'MBA'
+>>> contact
+'1234567'
+>>> 
+>>> 
+>>> 
+>>> # finding the average os 3 subject marks
+>>> # 3 subject marks
+>>> # o/p -> average of the 3 sub mrks
+>>> 
+>>> # arrange/collect/define the inputs / data
+>>> # compute - calculation
+>>> 
+>>> # displaying results
+>>> 
+>>> math=99
+>>> sci=89
+>>> soc=40
+>>> 
+>>> total=math+sci+soc
+>>> 
+>>> avg=total/3
+>>> 
+>>> avg
+76.0
+>>> 
+>>> 
+>>> 
+>>> # calculating simple Interest
+>>> 
+>>> # PTR/100
+>>> 
+>>> princi=10000
+>>> rate=8.5
+>>> time=5
+>>> 
+>>> roi=princi*time*rate/100
+>>> 
+>>> roi
+4250.0
+>>> 
+>>> # builtin functions
+>>> # function - collection of multiple commands stored in a variable/name
+>>> # takes input - output
+>>> 
+>>> # tesla()
+>>> # roi()
+>>> # sri()
+>>> 
+>>> 
+>>> # functionName(Input/Value)
+>>> 
+>>> # to find all the definations in the current session
+>>> 
+>>> # dir()
+>>> dir()
+['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__', 'age', 'avg', 'contact', 'edu', 'math', 'name', 'princi', 'rate', 'roi', 'sci', 'shareprice', 'soc', 'time', 'total']
+>>> 
+>>> # __somename__ / underscoreUNDERSCORE somename underscoreUNDERSCORE
+>>> # dunder methods - system
+>>> 
+>>> 
+>>> TRAINER = "SRI"
+>>> dir()
+['TRAINER', '__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__', 'age', 'avg', 'contact', 'edu', 'math', 'name', 'princi', 'rate', 'roi', 'sci', 'shareprice', 'soc', 'time', 'total']
+>>> 
+>>> pythontrainer="sri"
+>>> dir()
+['TRAINER', '__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__', 'age', 'avg', 'contact', 'edu', 'math', 'name', 'princi', 'pythontrainer', 'rate', 'roi', 'sci', 'shareprice', 'soc', 'time', 'total']
+>>> 
+>>> 
+>>> edu
+'MBA'
+>>> 
+>>> # datatype checking
+>>> # check what data is stored in a variable
+>>> 
+>>> # type()
+>>> # type(variableName/value)
+>>> 
+>>> type(edu)
+<class 'str'>
+>>> type(age)
+<class 'int'>
+>>> type(TRAINER)
+<class 'str'>
+>>> 
+>>> type(1111.1111)
+<class 'float'>
+>>> type(True)
+<class 'bool'>
+>>> 
+>>> 
+>>> 
