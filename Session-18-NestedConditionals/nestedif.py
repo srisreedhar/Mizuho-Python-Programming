085aa44c55db764388b25653cd4def847f867ea0
Sri Sreedhar <srisreedhar@live.com>
2020-05-06 14:28:56 +0530

    NestedConditions

---
+++ Session-18-NestedConditionals/nestedif.py
@@ -0,0 +1,24 @@
+# ask user to enter a number between 1-5 and print the number in words
+
+number=input("Enter a number between 1-5 :")
+number=int(number)
+
+# if number == 1:
+# 	print("the number is one")
+# else: 
+# 	print("its not one")
+
+# Nested conditions
+
+if number==1:
+	print("number is one")
+elif number==2:
+	print("number is two")
+elif number==3:
+	print("number is Three")
+elif number==4:
+	print("number is Four")
+elif number==5:
+	print("number is Five")
+else:
+	print("The number is out of range")	
