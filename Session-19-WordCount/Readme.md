57dd66ca8e22a34c850f800a22aec3d66f826a3a
Sri Sreedhar <srisreedhar@live.com>
2020-05-08 13:56:29 +0530

    WordCount

---
+++ Session-19-WordCount/Readme.md
@@ -0,0 +1,22 @@
+
+"this Morning and yesterdays MORNING are not same. it was raining yesterday and now it is not raining same as yesterday".lower()
+
+this - 1
+MORNING - 1
+Morning -1
+yesterdays -1
+raining - 2
+yesterday -1
+
+{
+	this : 1
+	morning : 2
+	yesterdays :1
+	raining : 2
+	yesterday :2
+	same:1+1
+}
+
+
+if there is no word - add word to dictionary and set its count as 1
+if theres the word exists, then increment the value by 1
---
+++ Session-19-WordCount/idleWorkOut.py
@@ -0,0 +1,59 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> "a"=="a"
+True
+>>> "A"=="a"
+False
+>>> 
+>>> "please go out"
+'please go out'
+
+>>> "PLEASE GO OUT"
+'PLEASE GO OUT'
+>>> for line in "william":
+	print(line)
+
+	
+w
+i
+l
+l
+i
+a
+m
+>>> 'romeo and juliet. '.split()
+['romeo', 'and', 'juliet.']
+>>> 
+>>> 
+>>> 
+>>> d={}
+>>> d['word']=1
+>>> 
+>>> d
+{'word': 1}
+>>> 
+>>> d['word']
+1
+>>> d['word']=d['word']+1
+>>> 
+>>> d['word']
+2
+>>> d['word']=d['word']+1
+>>> d['word']
+3
+>>> d['word']=d['word']+1
+>>> d['word']
+4
+>>> 
+>>> d
+{'word': 4}
+>>> 
+>>> 'sreedhar' in d
+False
+>>> 'word' in d
+True
+>>> 'sreedhar' not in d
+True
+>>> 'word' not in d
+False
+>>> 
